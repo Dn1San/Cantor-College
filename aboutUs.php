e89b52d6e239aedacc703e329f7708d6bdb4c57f
Dn1San <c0009613@hallam.shu.ac.uk>
2023-01-09 20:50:26 +0000

    CantorCollege

--- aboutUs.php
+++ aboutUs.php
@@ -12,29 +12,9 @@
 </head>
 <body>
     <div class="container">
-        <header>
-            <div id="logo">
-                <img src="cantor-college-website-images\logo-min.png" alt="Logo">
-                <div class="burger">
-                    <img src="cantor-college-website-images\whiteMenu.svg" alt="Menu">
-                </div>
-            </div>
-            <nav>
-                <ul>
-                    <li><a href="home.html">Home</a></li>
-                    <li><a href="aboutUs.html">About Us</a></li>
-                    <li><a href="findUs.html">How To Find Us</a></li>
-                    <li><a href="computingCourses.html">Computing Courses</a></li>
-                    <li><a href="designCourses.html">Design Courses</a></li>
-                    <li><a href="facilities.html">Facilitites</a></li>
-                    <li><a href="learningResources.html">Learning Rescourses</a></li>
-                    <li><a href="informationForStaff.html">Information For Staff</a></li>
-                    <li><a href="informationForStudents.html">Information For Students</a></li>
-                    <li><a href="workingWithBusiness.html">Working With Business</a></li>
-                    <li><a href="contactUs.html">Contact Us</a></li>
-                </ul> 
-            </nav>
-        </header>
+        <?php
+        include("includes/header.php");
+        ?>
         <div class="imageBanner">
             <img src="cantor-college-website-images\IMG_1437-min.jpeg" alt="Cantor Building">
         </div>
@@ -52,9 +32,9 @@
                 Cantor College, both as a student and in your future career.
             </p>
         </main>
-        <footer>
-            <p>&copy; Copyright 2022 Sheffield Hallam University</p>
-        </footer>
+        <?php
+        include("includes/footer.php");
+        ?>
     </div>
 </body>
 </html>
---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; Copyright 2022 Sheffield Hallam University</p>
+</footer>
