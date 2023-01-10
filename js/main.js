c90f07e2deb27d199557bbbcf797836c10fdf880
Dn1San <c0009613@hallam.shu.ac.uk>
2023-01-10 12:09:04 +0000

    Java Script

--- js/main.js
+++ js/main.js
@@ -4,6 +4,32 @@ $(document).ready(function(){
     $(".burger").on("click", function (){
         $("nav").slideToggle("fast")
     })
+    let setMenu = function () {
+        // variables
+        let burgerMenu = document.querySelector(".burger");
+        let navBar = document.querySelector("nav");
+        let navBarStatus = false;
+        if (navBar.style.left === "") {
+            burgerMenu.addEventListener("click", () => {
+            console.info(burgerMenu.classList);
+            if (navBarStatus) {
+                navBarStatus = false;
+                navBar.removeAttribute("style");
+                burgerMenu.setAttribute("class", "burger");
+            } else {
+                navBarStatus = true;
+                navBar.style.left = "0px";
+                burgerMenu.setAttribute("class", "burger changeBurger");
+            }
+            });
+        } else {
+            navBarStatus = false;
+            navBar.removeAttribute("style");
+        }
+    };
+    setMenu();
+    // resize
+    window.addEventListener("resize", () => setMenu());
 
     //Image Slider
     $("#lightSlider").lightSlider({
@@ -11,5 +37,6 @@ $(document).ready(function(){
         slideMargin: 0,
         loop: true,
     })
+
     //end
 })
